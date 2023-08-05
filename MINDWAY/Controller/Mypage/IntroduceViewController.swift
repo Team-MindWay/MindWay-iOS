d34550612bf79bbcee4fe90ef0cfead0fb0c03cf
Shin ain <s22028@gsm.hs.kr>
2023-08-05 23:20:24 +0900

    Modify IntroduceViewController Layout

--- MINDWAY/Controller/Mypage/IntroduceViewController.swift
+++ MINDWAY/Controller/Mypage/IntroduceViewController.swift
@@ -31,15 +31,17 @@ class IntroduceViewController: BaseViewController {
         topLogoImage.snp.makeConstraints {
             $0.width.equalTo(48)
             $0.height.equalTo(70)
-            $0.top.equalToSuperview().inset(40)
+            $0.top.equalTo(self.view.safeAreaInsets).offset(self.view.bounds.height * 0.045)
             $0.centerX.equalToSuperview()
         }
         titleView.snp.makeConstraints {
-            $0.top.equalTo(topLogoImage.snp.bottom).offset(40)
+            //$0.top.equalTo(topLogoImage.snp.bottom).offset(40)
+            $0.top.equalTo(self.view.safeAreaInsets).offset(self.view.bounds.height * 0.16)
             $0.centerX.equalToSuperview()
         }
         explainView.snp.makeConstraints {
-            $0.top.equalTo(titleView.titleLabel.snp.bottom).offset(70)
+            //$0.top.equalTo(titleView.titleLabel.snp.bottom).offset(50)
+            $0.top.equalTo(self.view.safeAreaInsets).offset(self.view.bounds.height * 0.26)
             $0.centerX.equalToSuperview()
         }
         topGradationView.snp.makeConstraints {
@@ -48,12 +50,12 @@ class IntroduceViewController: BaseViewController {
             $0.leading.trailing.equalToSuperview().inset(33)
         }
         teamMember.snp.makeConstraints {
-            $0.top.equalTo(topGradationView.snp.bottom).offset(20)
+            $0.top.equalTo(topGradationView.snp.bottom).offset(15)
             $0.leading.equalToSuperview().offset(115)
             //$0.leading.trailing.equalToSuperview().inset(99)
         }
         copyrightLabel.snp.makeConstraints {
-            $0.bottom.equalToSuperview().inset(120)
+            $0.bottom.equalToSuperview().multipliedBy(0.86)
             $0.centerX.equalToSuperview()
         }
     }
